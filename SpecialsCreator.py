96218e125dacc28be20894a6362d4bb6c7b59b03
Victor Ochoa <vochoa.developer@gmail.com>
2021-05-24 13:48:43 -0400

    Added TODO's to main program file, and started the basics

---
+++ SpecialsCreator.py
@@ -0,0 +1,16 @@
+numOfSpecials = input("Please enter the number of specials for today: ")
+numOfSpecials = int(numOfSpecials)
+
+i = 1
+while i <= numOfSpecials:
+    input(f"Enter special number {i}: ")
+    i += 1
+
+#TODO#
+#Find the corresponding specials in the database - MongoDB
+
+#Use MS Word template to populate
+#Create user friendly GUI
+
+#Automate updates to the WordPress and website?
+#Random generation of specials
