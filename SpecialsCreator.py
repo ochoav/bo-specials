4c788bc870436dff98cbe20c1600beb694b8b128
Victor Ochoa <vochoa.developer@gmail.com>
2021-06-01 22:38:11 -0400

    Implemented groundwork for context to be used in template

--- SpecialsCreator.py
+++ SpecialsCreator.py
@@ -3,10 +3,14 @@ import pymongo
 from pymongo import MongoClient
 from docxtpl import DocxTemplate
 
+#MongoDB database
 client = MongoClient()
 db = client.blackolive
+
+#Create template object
 doc = DocxTemplate("SpecialsTemplate.docx")
 
+#Use lunch or dinner collection
 while True:
     timeOfDay = input("Are you working on lunch or dinner specials?\n Type L or D: ")
     if "L" == timeOfDay.upper():
@@ -22,13 +26,17 @@ while True:
 numOfSpecials = input("Please enter the number of specials for today: ")
 numOfSpecials = int(numOfSpecials)
 
+#Get info for context to be used in template
+context = {}
 i = 1
 while i <= numOfSpecials:
     special = input(f"Enter special number {i}: ").upper()
 
     special = collection.find_one({"name": special})
-    print(special)
-
+    context[f"s{i}"] = special["name"]
+    context[f"s{i}_price"] = special["price"]
+    context[f"s{i}_description"] = special["description"]
+    
     i += 1
 
 #TODO#
